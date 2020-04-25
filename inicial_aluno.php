7e5bbb17398d8c13883c9af17e3acee1a0f70cfd
Thamyla Sousa Lima <thamyla.sl@gmail.com>
2020-04-25 17:17:54 -0300

    Foi inserido o Quiz na página inicial do aluno, ele já pode ser redirecionado para o mesmo

--- inicial_aluno.php
+++ inicial_aluno.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Inicial aluno</title>
-</head>
-<body>
-  <form method="POST" action="inicial_aluno.php">
-	<input type="submit" value="Criar sala" id="nova_sala" name="nova_sala">
-  </form>
-</body>
-</html>
-
 <?php
 //recebe o email do aluno como cookie da página login.php
 $email = $_COOKIE['email'];
@@ -23,7 +10,7 @@ $connect = mysqli_connect('localhost','root','admin');
 $db = mysqli_select_db($connect,'euklides');
 
 //seleciona o nome da sala em que o aluno está
-$query_select_aluno = "SELECT s.nome FROM aluno a, sala s WHERE a.email = '$email' AND
+$query_select_aluno = "SELECT s.nome, a.id FROM aluno a, sala s WHERE a.email = '$email' AND
 					s.id = a.sala_id";
 //$select_aluno recebe o resultado da execução da query
 $select_aluno = mysqli_query($connect,$query_select_aluno);
@@ -31,7 +18,28 @@ $select_aluno = mysqli_query($connect,$query_select_aluno);
 $array_aluno = mysqli_fetch_array($select_aluno);
 //$nome_sala recebe o nome da sala do aluno
 $nome_sala = $array_aluno['nome'];
+$cod_aluno = $array_aluno['id'];
 
 //apenas emite uma mensagem de qual sala o aluno está
-echo '<p>Você está na sala '.$nome_sala.'</p>';
+echo '<p>Voce esta na sala '.$nome_sala.'</p>';
 ?>
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Salas</title>
+</head>
+<body>
+  <table border="1">
+    <tr>
+      <td>Jogo</td>
+      <td>Descricao</td>
+    </tr>
+    <tr>
+      <td>
+      	<a href="quiz-game/index.html?<?php echo $cod_aluno; ?>">Quiz Game</a></td>
+      <td>Descricao do jogo</td>
+    </tr>
+  </table>
+</body>
+</html>
