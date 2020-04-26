fa305c43253be5716daa479cfbd8a4220ca2b629
Thamyla Sousa Lima <thamyla.sl@gmail.com>
2020-04-26 20:16:02 -0300

    Pela url do jogo, envia o código da sala também

--- inicial_aluno.php
+++ inicial_aluno.php
@@ -10,7 +10,7 @@ $connect = mysqli_connect('localhost','root','admin');
 $db = mysqli_select_db($connect,'euklides');
 
 //seleciona o nome da sala em que o aluno está
-$query_select_aluno = "SELECT s.nome, a.id FROM aluno a, sala s WHERE a.email = '$email' AND
+$query_select_aluno = "SELECT s.nome, a.id, a.sala_id FROM aluno a, sala s WHERE a.email = '$email' AND
 					s.id = a.sala_id";
 //$select_aluno recebe o resultado da execução da query
 $select_aluno = mysqli_query($connect,$query_select_aluno);
@@ -19,13 +19,14 @@ $array_aluno = mysqli_fetch_array($select_aluno);
 //$nome_sala recebe o nome da sala do aluno
 $nome_sala = $array_aluno['nome'];
 $cod_aluno = $array_aluno['id'];
+$cod_sala = $array_aluno['sala_id'];
 
 //apenas emite uma mensagem de qual sala o aluno está
 echo '<p>Voce esta na sala '.$nome_sala.'</p>';
 
 //seleciona os jogos disponíveis na sala
 $query_select_jogo = "SELECT j.id, j.nome FROM jogo j, sala s, sala_jogo sj 
-WHERE s.nome = '$nome_sala' AND s.id = sj.sala_id AND j.id = sj.jogo_id";
+WHERE s.id = '$cod_sala' AND s.id = sj.sala_id AND j.id = sj.jogo_id";
 //$select_jogo recebe o resultado da execução da query
 $select_jogo = mysqli_query($connect,$query_select_jogo);
 ?>
@@ -46,7 +47,7 @@ $select_jogo = mysqli_query($connect,$query_select_jogo);
     while($dado_jogo = $select_jogo->fetch_array()) { ?>
     <tr>
 	    <td>
-	      <a href="<?php echo $dado_jogo['id']; ?>/index.html?<?php echo $cod_aluno; ?>">
+	      <a href="<?php echo $dado_jogo['id']; ?>/index.html?<?php echo $cod_aluno; ?>,<?php echo $cod_sala; ?>">
 	      <?php echo $dado_jogo['nome']; ?></a></td>
 	    <td>Descricao do jogo</td>
     </tr>
