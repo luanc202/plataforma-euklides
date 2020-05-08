12a51e95e438543f6684c35230ea59d86994deb1
Thacyla Lima <thacyla.sl@gmail.com>
2020-05-08 11:28:49 -0300

    Adição da imagem do jogo Quiz. A imagem deve ter o mesmo nome do jogo.

--- inicial_aluno.php
+++ inicial_aluno.php
@@ -1,19 +1,19 @@
 <?php
 include 'header.php';
-//recebe o email do aluno como cookie da página login.php
+//recebe o email do aluno como cookie da pï¿½gina login.php
 $email = $_COOKIE['email'];
 //exclui o cookie recebido
 unset($_COOKIE['email']);
 
-//cria a conexão
+//cria a conexï¿½o
 $connect = mysqli_connect('localhost','root','admin');
 //seleciona o banco de dados euklides
 $db = mysqli_select_db($connect,'euklides');
 
-//seleciona o nome da sala em que o aluno está
+//seleciona o nome da sala em que o aluno estï¿½
 $query_select_aluno = "SELECT s.nome, a.cod_aluno, a.sala_id FROM aluno a, sala s WHERE a.email = '$email' AND
 					s.cod_sala = a.sala_id";
-//$select_aluno recebe o resultado da execução da query
+//$select_aluno recebe o resultado da execuï¿½ï¿½o da query
 $select_aluno = mysqli_query($connect,$query_select_aluno);
 //$array_aluno recebe todos os dados recebidos
 $array_aluno = mysqli_fetch_array($select_aluno);
@@ -22,10 +22,10 @@ $nome_sala = $array_aluno['nome'];
 $cod_aluno = $array_aluno['cod_aluno'];
 $cod_sala = $array_aluno['sala_id'];
 
-//seleciona os jogos disponíveis na sala
+//seleciona os jogos disponï¿½veis na sala
 $query_select_jogo = "SELECT j.cod_jogo, j.nome FROM jogo j, sala s, sala_jogo sj 
 WHERE s.cod_sala = '$cod_sala' AND s.cod_sala = sj.sala_id AND j.cod_jogo = sj.jogo_id";
-//$select_jogo recebe o resultado da execução da query
+//$select_jogo recebe o resultado da execuï¿½ï¿½o da query
 $select_jogo = mysqli_query($connect,$query_select_jogo);
 ?>
 
@@ -42,11 +42,11 @@ $select_jogo = mysqli_query($connect,$query_select_jogo);
 		<h2><?php echo $nome_sala; ?></h2>
 	  	
 	  	<?php 
-	  	//enquanto houver jogos, são criada as divs
+	  	//enquanto houver jogos, sï¿½o criada as divs
 	  	while($dado_jogo = $select_jogo->fetch_array()) {?>
 	  	<div class="div_jogo">
 	  		<a href="<?php echo $dado_jogo['cod_jogo']; ?>/index.html?<?php echo $cod_aluno; ?>,<?php echo $cod_sala; ?>">
-	  			<img class="imagem_jogo" src="imgs/jogo.jpg" alt="some text" ></a>
+	  			<img class="imagem_jogo" src="imgs/<?php echo $dado_jogo['nome']; ?>.jpg" alt="some text" ></a>
 	  		<a href="<?php echo $dado_jogo['cod_jogo']; ?>/index.html?<?php echo $cod_aluno; ?>,<?php echo $cod_sala; ?>">
 	  		<p class="nome_jogo"><?php echo $dado_jogo['nome']; ?></p></a>
 	  		<a class="link_avaliar" href="">Avaliar jogo</a>
